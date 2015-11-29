99dac702eeb623a2e231196574c97a8122a7f274
dd <dd>
2015-11-29 11:03:41 +0200

    fatura

--- TutevERP/src/main/java/org/tutev/web/erp/controller/fatura/FaturaController.java
+++ TutevERP/src/main/java/org/tutev/web/erp/controller/fatura/FaturaController.java
@@ -1,39 +1,55 @@
 package org.tutev.web.erp.controller.fatura;
 
-import javax.annotation.PostConstruct;
+import java.io.Serializable;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.tutev.web.erp.entity.fatura.Fatura;
+import org.tutev.web.erp.service.FaturaService;
 
 
 @Controller("faturaController")
 @Scope("session")
-public class FaturaController {
+public class FaturaController implements Serializable{
 
-	private int faturaId;
-	private String faturaAdi;
-	
-	
+	@Autowired
+	private transient FaturaService  faturaService;
+
+	private Fatura fatura;
+	List<Fatura> faturaListesi;
+
 	@PostConstruct
-	private void init() {
-		setFaturaAdi("Fatura1");
-		setFaturaId(1234);
+	public void init() {
+		FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+		faturaListesi=faturaService.getAll();
+	}
+
+	public void faturaKaydet() {
+		faturaService.save(fatura);
+
+		faturaListesi=faturaService.getAll();
+		fatura=null;
+	}
+	
+	public List<Fatura> getFaturaListesi() {
+		return faturaListesi;
+	}
+
+	public Fatura getFatura() {
+	 if(fatura==null)
+		 fatura=new Fatura();
+		return fatura;
+	}
+
+	public void setFatura(Fatura fatura) {
 		
+		this.fatura = fatura;
 	}
-	
-	public int getFaturaId() {
-		return faturaId;
-	}
-	public void setFaturaId(int faturaId) {
-		this.faturaId = faturaId;
-	}
-	public String getFaturaAdi() {
-		return faturaAdi;
-	}
-	public void setFaturaAdi(String faturaAdi) {
-		this.faturaAdi = faturaAdi;
-	}
-	
 
 	
 	
