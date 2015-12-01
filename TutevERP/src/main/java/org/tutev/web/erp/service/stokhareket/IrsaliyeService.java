d7fb9db46f7ee682efe7b8a5e8a628f1600a4397
Mehmet Emin IŞIK <mehmet.emin.isik@gmail.com>
2015-12-01 22:17:45 +0200

    Irsaliye Service changes for lazy loading

--- TutevERP/src/main/java/org/tutev/web/erp/service/stokhareket/IrsaliyeService.java
+++ TutevERP/src/main/java/org/tutev/web/erp/service/stokhareket/IrsaliyeService.java
@@ -4,12 +4,17 @@
 package org.tutev.web.erp.service.stokhareket;
 
 import java.util.List;
+import java.util.Map;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
 import org.springframework.stereotype.Service;
 import org.tutev.web.erp.service.BaseDao;
 import org.tutev.web.erp.service.ServiceBase;
+import org.tutev.web.erp.util.PageingModel;
+import org.tutev.web.erp.entity.genel.Kisi;
 import org.tutev.web.erp.entity.stokhareket.Irsaliye;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -27,6 +32,8 @@ public class IrsaliyeService implements ServiceBase<Irsaliye> {
 	
 	@Override
 	public Irsaliye save(Irsaliye entity) {
+		if (entity==null)
+			return null;
 		baseDao.save(entity);
 		return entity;
 	}
@@ -58,11 +65,23 @@ public class IrsaliyeService implements ServiceBase<Irsaliye> {
 	@Override
 	public List<Irsaliye> getAll() {
 		Criteria criteria = getSession().createCriteria(Irsaliye.class);
+		criteria.addOrder(Order.desc("tarih"));
 		return (List<Irsaliye>) criteria.list();
 	}
 
 	@Override
 	public Session getSession() {
 		return baseDao.getSession();
+	}
+
+	@SuppressWarnings("unchecked")
+	public PageingModel<Irsaliye> getByPageing(int first, int pageSize, Map<String, Object> filters) {
+		Criteria criteria = getSession().createCriteria(Irsaliye.class);
+		criteria.setMaxResults(pageSize);
+		criteria.setFirstResult(first);
+		criteria.addOrder(Order.desc("id"));
+		List<Irsaliye> list= criteria.list();
+		int kayitSayisi=((Long)getSession().createCriteria(Irsaliye.class).setProjection(Projections.rowCount()).uniqueResult()).intValue();
+		return new PageingModel<Irsaliye>(list, kayitSayisi);
 	}
 }
